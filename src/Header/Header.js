0ad481fb4324fc94c444f3b15c27fe8d894c34bb
Jordan Skomal <jskomal@gmail.com>
2022-03-23 17:07:58 -0600

    Add JSX structures

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,7 +2,19 @@ import React, { Component } from 'react'
 
 export class Header extends Component {
   render() {
-    return <div>Header</div>
+    return (
+      <div>
+        <h1>rancid tomatillos</h1>
+        <button>home</button>
+        <input
+          type='text'
+          name='search'
+          id='search'
+          placeholder='search for a movie title'
+        />
+        <button>log in</button>
+      </div>
+    )
   }
 }
 
