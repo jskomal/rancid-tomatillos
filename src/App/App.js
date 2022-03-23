0a6275777ac1c8e7e8bc2d1f054c52293b1a10cf
Jordan Skomal <jskomal@gmail.com>
2022-03-23 16:57:43 -0600

    Create and Import movieData

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
+import movieData from '../movieData'
 
 export class App extends Component {
   render() {
-    return <div>App</div>
+    return <div></div>
   }
 }
 
