fcd4498bf27dff99e4024617e5ceca77b5fb59de
Jordan Skomal <jskomal@gmail.com>
2022-03-23 17:03:07 -0600

    Create Header

--- src/App/App.js
+++ src/App/App.js
@@ -2,8 +2,19 @@ import React, { Component } from 'react'
 import movieData from '../movieData'
 
 export class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      movieData: movieData
+    }
+  }
+
   render() {
-    return <div></div>
+    return (
+      <div>
+        <Header movieData={movieData} />
+      </div>
+    )
   }
 }
 
