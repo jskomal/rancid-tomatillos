a4a1eacf9c77dd538c112a3048ab74f623e1ffcc
Jordan Skomal <jskomal@gmail.com>
2022-03-23 18:35:43 -0600

    Refactor Main component

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,10 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './Main.css'
+import Card from '../Card/Card'
 
-export class Main extends Component {
-  render() {
-    return <div>Main</div>
-  }
+const Main = ({ movies }) => {
+  const cards = movies.map((movie) => (
+    <Card
+      poster_path={movie.poster_path}
+      title={movie.title}
+      average_rating={movie.average_rating}
+      release_date={movie.release_date}
+      key={movie.id}
+    />
+  ))
+
+  return <main className='main-view'>{cards}</main>
 }
 
 export default Main
