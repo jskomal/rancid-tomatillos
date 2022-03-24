4e356d3e5e7c6d1a9ae8733c599a8c79bc5fa5c7
danielsolomon332 <daniel.solomon332@gmail.com>
2022-03-24 17:16:27 -0600

    Add onClick event to toggle views

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react'
 import './Header.css'
 
 export class Header extends Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return (
       <header>
         <div className='logo-pair'>
           <h1 className='logo-title pair-item'>rancid tomatillos</h1>
-          <button className='home-button pair-item'>home</button>
+          <button className='home-button pair-item' name='home' onClick={this.props.toggleView}>home</button>
         </div>
         <input
           className='search'
