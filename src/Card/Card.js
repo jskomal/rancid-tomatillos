bd38aca68d97aabea1fd33b502101bad7cbbfb99
Jordan Skomal <jskomal@gmail.com>
2022-03-24 09:35:05 -0600

    Style detail names

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -7,8 +7,14 @@ const Card = ({ poster_path, title, average_rating, release_date }) => {
       <img className='poster-img' src={poster_path} alt={`${title} poster`} />
       <h2 className='poster-title'>{title}</h2>
       <section className='poster-footer'>
-        <h3 className='footer-item-l'>{`rating: ${average_rating.toFixed(2)}`}</h3>
-        <h3 className='footer-r'>{`released in ${release_date.substring(0, 4)}`}</h3>
+        <div className='detail-pair'>
+          <h3 className='footer-item-l label'>rating:</h3>
+          <h3 className='footer-item-l'>{average_rating.toFixed(2)}</h3>
+        </div>
+        <div className='detail-pair'>
+          <h3 className='footer-r label'>released in:</h3>
+          <h3 className='footer-r'>{release_date.substring(0, 4)}</h3>
+        </div>
       </section>
     </article>
   )
