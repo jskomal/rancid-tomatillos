01068a50d92af44d26bdba2febda39c67dbe8b05
Jordan Skomal <jskomal@gmail.com>
2022-03-23 18:59:35 -0600

    Refactor class names

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -7,8 +7,8 @@ const Card = ({ poster_path, title, average_rating, release_date }) => {
       <img className='poster-img' src={poster_path} alt={`${title} poster`} />
       <h2 className='poster-title'>{title}</h2>
       <section className='poster-footer'>
-        <h3 className='footer-item'>{`rating: ${average_rating.toFixed(2)}`}</h3>
-        <h3 className='footer-item'>{`released in ${release_date.substring(0, 4)}`}</h3>
+        <h3 className='footer-item-l'>{`rating: ${average_rating.toFixed(2)}`}</h3>
+        <h3 className='footer-r'>{`released in ${release_date.substring(0, 4)}`}</h3>
       </section>
     </article>
   )
