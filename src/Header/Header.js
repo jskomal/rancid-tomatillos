3237c269a5b1d83bbc91227798fadb6282216091
Jordan Skomal <jskomal@gmail.com>
2022-03-26 17:04:52 -0600

    Style Header buttons

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import './Header.css'
 
 export class Header extends Component {
-  constructor(props) {
-    super(props)
+  constructor() {
+    super()
     this.state = {
       searchValue: ''
     }
