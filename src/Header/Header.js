fcf222cd80b74c713477a6cf739322891c8672d8
Jordan Skomal <jskomal@gmail.com>
2022-03-29 16:10:03 -0600

    Add Link to home button

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import './Header.css'
 
 export class Header extends Component {
@@ -19,13 +20,11 @@ export class Header extends Component {
       <header>
         <div className='logo-pair'>
           <h1 className='logo-title pair-item'>rancid tomatillos</h1>
-          <button
-            className='home-button pair-item'
-            name='home'
-            onClick={this.props.toggleView}
-          >
-            home
-          </button>
+          <Link to='/'>
+            <button className='home-button pair-item' name='home'>
+              home
+            </button>
+          </Link>
         </div>
         {!this.props.isSingleView && (
           <input
