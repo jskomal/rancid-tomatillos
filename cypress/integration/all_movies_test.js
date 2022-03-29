ddb6f91ce54eab8517f4383964d5dfdd12f6a820
Jordan Skomal <jskomal@gmail.com>
2022-03-29 13:41:52 -0600

    Add stubbing for tests

--- cypress/integration/all_movies_test.js
+++ cypress/integration/all_movies_test.js
@@ -1,6 +1,42 @@
 describe('All Movies View Tests', () => {
   beforeEach(() => {
-    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/')
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/', {
+      statusCode: 201,
+      body: {
+        movies: [
+          {
+            id: 337401,
+            poster_path:
+              'https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg',
+            backdrop_path:
+              'https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg',
+            title: 'Mulan',
+            average_rating: 5.2727272727272725,
+            release_date: '2020-09-04'
+          },
+          {
+            id: 340102,
+            poster_path:
+              'https://image.tmdb.org/t/p/original//xrI4EnZWftpo1B7tTvlMUXVOikd.jpg',
+            backdrop_path:
+              'https://image.tmdb.org/t/p/original//eCIvqa3QVCx6H09bdeOS8Al2Sqy.jpg',
+            title: 'The New Mutants',
+            average_rating: 4,
+            release_date: '2020-08-26'
+          },
+          {
+            id: 737173,
+            poster_path:
+              'https://image.tmdb.org/t/p/original//opZKcgocttEOAUzqluX3bUbbDew.jpg',
+            backdrop_path: 'https://www.esm.rochester.edu/uploads/NoPhotoAvailable.jpg',
+            title: 'Maratón After',
+            average_rating: 4.333333333333333,
+            release_date: '2020-09-03'
+          }
+        ]
+      }
+    }).as('movies')
+
     cy.visit('http://localhost:3000/')
   })
 
@@ -31,9 +67,8 @@ describe('All Movies View Tests', () => {
   })
 
   it('Should be able to type in the search bar and filter movie results', () => {
-    cy.wait(2000) // how to use aliasing to wait one the GET is finished
     cy.get('.search').type('Mulan').get('img[alt="Mulan poster"]').should('be.visible')
   })
 
-  // add test for api server not responding (test error message) (will implement after Routing)
+  // add test for api server not responding (test error message) (will implement after Routing) and add aliasing waits for consistent results
 })
--- cypress/integration/single_movie_test.js
+++ cypress/integration/single_movie_test.js
@@ -1,13 +1,91 @@
 describe('Single Movie View tests', () => {
   beforeEach(() => {
-    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/')
-      .visit('http://localhost:3000/')
-      .get('img[alt="The New Mutants poster"]')
-      .click()
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/', {
+      statusCode: 201,
+      body: {
+        movies: [
+          {
+            id: 337401,
+            poster_path:
+              'https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg',
+            backdrop_path:
+              'https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg',
+            title: 'Mulan',
+            average_rating: 5.2727272727272725,
+            release_date: '2020-09-04'
+          },
+          {
+            id: 340102,
+            poster_path:
+              'https://image.tmdb.org/t/p/original//xrI4EnZWftpo1B7tTvlMUXVOikd.jpg',
+            backdrop_path:
+              'https://image.tmdb.org/t/p/original//eCIvqa3QVCx6H09bdeOS8Al2Sqy.jpg',
+            title: 'The New Mutants',
+            average_rating: 4,
+            release_date: '2020-08-26'
+          },
+          {
+            id: 737173,
+            poster_path:
+              'https://image.tmdb.org/t/p/original//opZKcgocttEOAUzqluX3bUbbDew.jpg',
+            backdrop_path: 'https://www.esm.rochester.edu/uploads/NoPhotoAvailable.jpg',
+            title: 'Maratón After',
+            average_rating: 4.333333333333333,
+            release_date: '2020-09-03'
+          }
+        ]
+      }
+    }).as('movies')
+
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/340102', {
+      statusCode: 201,
+      body: {
+        movie: {
+          id: 340102,
+          title: 'The New Mutants',
+          poster_path:
+            'https://image.tmdb.org/t/p/original//xrI4EnZWftpo1B7tTvlMUXVOikd.jpg',
+          backdrop_path:
+            'https://image.tmdb.org/t/p/original//eCIvqa3QVCx6H09bdeOS8Al2Sqy.jpg',
+          release_date: '2020-08-26',
+          overview:
+            'Five young mutants, just discovering their abilities while held in a secret facility against their will, fight to escape their past sins and save themselves.',
+          genres: ['Action', 'Science Fiction', 'Horror', 'Adventure'],
+          budget: 67000000,
+          revenue: 3100000,
+          runtime: 94,
+          tagline: "It's time to face your demons",
+          average_rating: 4
+        }
+      }
+    }).as('mutants')
+
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/737173', {
+      statusCode: 201,
+      body: {
+        movie: {
+          id: 737173,
+          title: 'Maratón After',
+          poster_path:
+            'https://image.tmdb.org/t/p/original//opZKcgocttEOAUzqluX3bUbbDew.jpg',
+          backdrop_path: 'https://www.esm.rochester.edu/uploads/NoPhotoAvailable.jpg',
+          release_date: '2020-09-03',
+          overview: '',
+          genres: [],
+          budget: 0,
+          revenue: 0,
+          runtime: 0,
+          tagline: '',
+          average_rating: 4.333333333333333
+        }
+      }
+    }).as('maraton')
+
+    cy.visit('http://localhost:3000/').get('img[alt="The New Mutants poster"]').click()
   })
 
   it('should display the same card from the main page', () => {
-    cy.get('img[alt="Mulan poster"]').should('be.visible')
+    cy.get('img[alt="The New Mutants poster"]').should('be.visible')
 
     cy.get('.poster-title').contains('The New Mutants')
 
@@ -30,10 +108,11 @@ describe('Single Movie View tests', () => {
   })
 
   it('can also render no tagline, overview, etc', () => {
-    cy.wait(2000) // same issue as before, how to correctly use aliasing to wait dynamically
     cy.get('.home-button').click()
 
     cy.get('img[alt="Maratón After poster"]').click()
+
+    cy.wait('@maraton')
 
     cy.get('.movie-detail-error').contains('No information is available for this movie')
   })
