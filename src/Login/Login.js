dae39bf1c530df23cd54247db6ebce3d0d73ffb1
danielsolomon332 <daniel.solomon332@gmail.com>
2022-03-30 17:27:53 -0600

    Add login fields

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -4,7 +4,25 @@ import './Login.css'
 export class Login extends Component {
   render() {
     return (
-      <div>Login</div>
+      <div className='login-view'>
+        <h3 className='login-title'>Log In</h3>
+        <input
+          className='login-input'
+          type='text'
+          name='email'
+          placeholder='email address'
+          value={ this.state.email }
+          // add on change
+        />
+        <input
+          className='login-input'
+          type='password'
+          name='password'
+          placeholder='password'
+          value={ this.state.password }
+          // add on change
+        />
+      </div>
     )
   }
 }
