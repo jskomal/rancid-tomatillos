a2ae0412f269a1891e39138eef54da3ee0dd83ec
danielsolomon332 <daniel.solomon332@gmail.com>
2022-03-30 17:16:14 -0600

    Add required files for Login

---
+++ src/Login/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Login.css'
+
+export class Login extends Component {
+  render() {
+    return (
+      <div>Login</div>
+    )
+  }
+}
+
+export default Login
