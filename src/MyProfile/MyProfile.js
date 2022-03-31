2d99ddebb0856e01be17bdf2baa63006bd26820d
Jordan Skomal <jskomal@gmail.com>
2022-03-31 15:48:35 -0600

    Refactor GET Fetch for user reviews

--- src/MyProfile/MyProfile.js
+++ src/MyProfile/MyProfile.js
@@ -2,48 +2,70 @@ import React, { Component } from 'react'
 import './MyProfile.css'
 import RatingCard from '../RatingCard/RatingCard'
 import ToRate from '../ToRate/ToRate'
+import { fetchDataGet, fetchDataDelete } from '../APICalls'
 
 export class MyProfile extends Component {
   constructor(props) {
     super(props)
-    this.state = { userData: props.userData, ratings: null }
+    this.state = {
+      userData: props.userData,
+      ratings: null,
+      ratingCards: null,
+      errorMsg: ''
+    }
   }
 
-  fetchData = (path) => {
-    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/${path}`)
+  createRatingCards = () => {
+    fetchDataGet(`/users/${this.state.userData.id}/ratings`)
       .then((res) => {
         if (!res.ok) {
           this.setState({ errorMsg: 'Something went wrong, try again later' })
         }
         return res.json()
       })
+      .then((data) => {
+        this.setState({ ratings: data.ratings })
+      })
+  }
+
+  componentDidMount() {
+    this.createRatingCards()
+  }
+
+  deleteRating = (idToDelete) => {
+    fetch(
+      `https://rancid-tomatillos.herokuapp.com/api/v2/users/${this.state.userData.id}/ratings/${idToDelete}`,
+      {
+        method: 'DELETE'
+      }
+    )
+      .then((res) => {
+        console.log(res)
+        this.createRatingCards()
+      })
       .catch((error) => {
         throw new Error(error)
       })
   }
 
-  componentDidMount() {
-    this.fetchData(`/users/${this.state.userData.id}/ratings`).then((data) => {
-      this.setState({
-        ratings: data.ratings,
-        ratingCards: data.ratings.map((review) => {
-          return (
-            <RatingCard
-              key={review.id}
-              movie_id={review.movie_id}
-              rating={review.rating}
-              dateUpdated={review.updated_at}
-              movies={this.props.movies}
-            />
-          )
-        })
-      })
-    })
-  }
-
-  deleteRating = (id) => {}
-
   render() {
+    let ratingCards = []
+    if (this.state.ratings) {
+      ratingCards = this.state.ratings.map((review) => {
+        return (
+          <RatingCard
+            key={review.id}
+            id={review.id}
+            movie_id={review.movie_id}
+            rating={review.rating}
+            dateUpdated={review.updated_at}
+            movies={this.props.movies}
+            deleteRating={this.deleteRating}
+          />
+        )
+      })
+    }
+
     return (
       <div className='profile-view'>
         <div className='profile-title'>
@@ -56,9 +78,7 @@ export class MyProfile extends Component {
           )}
           <h4>Your Ratings</h4>
         </div>
-        <div className='ratings-view'>
-          {this.state.ratings && <>{this.state.ratingCards}</>}
-        </div>
+        <div className='ratings-view'>{<>{ratingCards}</>}</div>
       </div>
     )
   }
