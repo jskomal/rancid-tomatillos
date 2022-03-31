51ccecca64e1e32be6cbe25a1221919f67726cda
Jordan Skomal <jskomal@gmail.com>
2022-03-31 15:46:41 -0600

    Refactor all API Calls to come from the exported function

--- src/APICalls.js
+++ src/APICalls.js
@@ -1,7 +1,28 @@
-const fetchData = (path) => {
-  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/${path}`).then((res) => {
-    return res.json()
+const fetchDataGet = (path) => {
+  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/${path}`).catch(
+    (error) => {
+      throw new Error(error)
+    }
+  )
+}
+const fetchDataDelete = (path) => {
+  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/${path}`, {
+    method: 'DELETE'
   })
 }
 
-export { fetchData }
+export { fetchDataGet, fetchDataDelete }
+
+// ratingCards: data.ratings.map((review) => {
+//   return (
+//     <RatingCard
+//       key={review.id}
+//       id={review.id}
+//       movie_id={review.movie_id}
+//       rating={review.rating}
+//       dateUpdated={review.updated_at}
+//       movies={this.props.movies}
+//       deleteRating={this.deleteRating}
+//     />
+//   )
+// })
