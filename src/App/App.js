ab87ea0713957d518874b630c42a2e63468c4272
Jordan Skomal <jskomal@gmail.com>
2022-03-31 15:47:04 -0600

    Refactor API calls and error messaging

--- src/App/App.js
+++ src/App/App.js
@@ -6,6 +6,7 @@ import Login from '../Login/Login'
 import MyProfile from '../MyProfile/MyProfile'
 import './App.css'
 import { Route, Switch } from 'react-router-dom'
+import { fetchDataGet } from '../APICalls'
 
 export class App extends Component {
   constructor() {
@@ -16,30 +17,38 @@ export class App extends Component {
       isLoading: true,
       isError: false,
       userData: null,
-      isLoggedIn: false
+      isLoggedIn: false,
+      errorMsg: ''
     }
   }
 
-  fetchData = (path) => {
-    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/${path}`).then((res) => {
-      this.setState({ isLoading: true })
-      return res.json()
-    })
-  }
-
   componentDidMount() {
-    this.fetchData('movies')
+    fetchDataGet('movies')
+      .then((res) => {
+        if (!res.ok) {
+          this.setState(
+            {
+              isError: true,
+              errorMsg: 'Server Error, try Rotten Tomatoes instead'
+            },
+            () => {
+              throw new Error(res.status)
+            }
+          )
+        }
+        return res.json()
+      })
       .then((data) => {
         this.setState({
+          isLoading: false,
           movies: data.movies,
-          filteredMovies: data.movies,
-          isLoading: false
+          filteredMovies: data.movies
         })
       })
-      .catch((error) => {
-        this.setState({ isError: true })
-        throw new Error(error)
-      })
+  }
+
+  finishLoading = () => {
+    this.setState({ isLoading: false })
   }
 
   filterMovies = (searchTerm) => {
@@ -75,9 +84,7 @@ export class App extends Component {
                   isLoggedIn={this.state.isLoggedIn}
                 />
                 {this.state.isError && (
-                  <h1 className='status-msg'>
-                    Server Error, try Rotten Tomatoes instead
-                  </h1>
+                  <h1 className='status-msg'>{this.state.errorMsg}</h1>
                 )}
                 {this.state.isLoading && !this.state.isError && (
                   <h1 className='status-msg'>Loading... Grab some popcorn!</h1>
@@ -101,7 +108,13 @@ export class App extends Component {
                   location={location.pathname}
                   isLoggedIn={this.state.isLoggedIn}
                 />
-                <Login toggleLogInStatus={this.toggleLogInStatus} />
+                {this.state.isError && (
+                  <h1 className='status-msg'>{this.state.errorMsg}</h1>
+                )}
+                <Login
+                  finishLoading={this.finishLoading}
+                  toggleLogInStatus={this.toggleLogInStatus}
+                />
               </div>
             )
           }}
@@ -118,7 +131,14 @@ export class App extends Component {
                   location={location.pathname}
                   isLoggedIn={this.state.isLoggedIn}
                 />
-                <MyProfile userData={this.state.userData} movies={this.state.movies} />
+                {this.state.isError && (
+                  <h1 className='status-msg'>{this.state.errorMsg}</h1>
+                )}
+                <MyProfile
+                  finishLoading={this.finishLoading}
+                  userData={this.state.userData}
+                  movies={this.state.movies}
+                />
               </>
             )
           }}
@@ -136,11 +156,12 @@ export class App extends Component {
                   isLoggedIn={this.state.isLoggedIn}
                 />
                 {this.state.isError && (
-                  <h1 className='status-msg'>
-                    Server Error, try Rotten Tomatoes instead
-                  </h1>
+                  <h1 className='status-msg'>{this.state.errorMsg}</h1>
                 )}
-                <SingleView currentMovieID={match.params} />
+                <SingleView
+                  finishLoading={this.finishLoading}
+                  currentMovieID={match.params}
+                />
               </div>
             )
           }}
