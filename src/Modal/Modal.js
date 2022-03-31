e57c1524baf7d0e06d3343227e953e554001adaa
danielsolomon332 <daniel.solomon332@gmail.com>
2022-03-31 16:50:54 -0600

    Create Modal component structure

---
+++ src/Modal/Modal.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './Modal.css'
+
+export class Modal extends Component {
+  constructor() {
+    super()
+    this.state = { ratingValue: ''}
+  }
+
+  render() {
+    return <div>Modal</div>
+  }
+}
+
+export default Modal
