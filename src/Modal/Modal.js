e8bbec5491909718ed11037efa342bfdf15e9d2b
danielsolomon332 <daniel.solomon332@gmail.com>
2022-03-31 17:07:59 -0600

    Add handle Input method and modal details

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -4,11 +4,21 @@ import './Modal.css'
 export class Modal extends Component {
   constructor() {
     super()
-    this.state = { ratingValue: ''}
+    this.state = { ratingValue: 1 }
+  }
+
+  handleInput = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
   }
 
   render() {
-    return <div>Modal</div>
+    return(
+    <div className='modal'>
+      <h2>add your rating</h2>
+      <p>out of 10</p>
+      <input type='number' name='ratingValue' min='1' max='10' step='1' value={ this.state.ratingValue } onChange={ this.handleInput }/>
+      <button onClick={ e => this.props.addRating(this.state.ratingValue) }>submit rating</button>
+    </div>)
   }
 }
 
