e09e1e6c4501285bae04fc75cf11962ff4a7b847
Jordan Skomal <jskomal@gmail.com>
2022-03-30 18:39:40 -0600

    Create MyProfile files

---
+++ src/MyProfile/MyProfile.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './MyProfile.css'
+
+export class MyProfile extends Component {
+  render() {
+    return <div>MyProfile</div>
+  }
+}
+
+export default MyProfile
