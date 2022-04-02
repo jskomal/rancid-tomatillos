96324c3704411822889b3841407d9b3f52a71a2e
danielsolomon332 <daniel.solomon332@gmail.com>
2022-04-02 13:41:00 -0600

    Refactor intercept

--- cypress/integration/all_movies_test.js
+++ cypress/integration/all_movies_test.js
@@ -1,6 +1,46 @@
 describe('All Movies View Tests', () => {
-  beforeEach(() => {
-    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/', {
+  // beforeEach(() => {
+    // cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/', {
+    //   statusCode: 201,
+    //   body: {
+    //     movies: [
+    //       {
+    //         id: 337401,
+    //         poster_path:
+    //           'https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg',
+    //         backdrop_path:
+    //           'https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg',
+    //         title: 'Mulan',
+    //         average_rating: 5.2727272727272725,
+    //         release_date: '2020-09-04'
+    //       },
+    //       {
+    //         id: 340102,
+    //         poster_path:
+    //           'https://image.tmdb.org/t/p/original//xrI4EnZWftpo1B7tTvlMUXVOikd.jpg',
+    //         backdrop_path:
+    //           'https://image.tmdb.org/t/p/original//eCIvqa3QVCx6H09bdeOS8Al2Sqy.jpg',
+    //         title: 'The New Mutants',
+    //         average_rating: 4,
+    //         release_date: '2020-08-26'
+    //       },
+    //       {
+    //         id: 737173,
+    //         poster_path:
+    //           'https://image.tmdb.org/t/p/original//opZKcgocttEOAUzqluX3bUbbDew.jpg',
+    //         backdrop_path: 'https://www.esm.rochester.edu/uploads/NoPhotoAvailable.jpg',
+    //         title: 'Maratón After',
+    //         average_rating: 4.333333333333333,
+    //         release_date: '2020-09-03'
+    //       }
+    //     ]
+    //   }
+    // }).as('movies')
+  // })
+
+  it('Should have a header with a title, home button, search bar, and log in button', () => {
+    cy.wait(2000)
+    cy.intercept('/movies', {
       statusCode: 201,
       body: {
         movies: [
@@ -36,11 +76,9 @@ describe('All Movies View Tests', () => {
         ]
       }
     }).as('movies')
-  })
 
-  it('Should have a header with a title, home button, search bar, and log in button', () => {
     cy.visit('http://localhost:3000/')
-    
+
     cy.get('header').contains('rancid tomatillos')
 
     cy.get('.home-button').contains('home')
@@ -70,5 +108,12 @@ describe('All Movies View Tests', () => {
     cy.get('.search').type('Mulan').get('img[alt="Mulan poster"]').should('be.visible')
   })
 
-  // add test for api server not responding (test error message) (will implement after Routing) and add aliasing waits for consistent results
+  it('should render an error message if the server is not reachable', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/',
+    {forceNetworkError: true}
+  ).as('serverError')
+  cy.visit('http://localhost:3000/')
+  // .wait('@serverError')
+  .get('.page-container').contains('Server Error, try Rotten Tomatoes instead')
+  })
 })
