3eca6fe337ff3b676b34cb25f76585b79997ee42
danielsolomon332 <daniel.solomon332@gmail.com>
2022-04-03 15:15:44 -0600

    Refactor fixture

--- cypress/integration/login_test.js
+++ cypress/integration/login_test.js
@@ -2,39 +2,7 @@ describe('Login Flow Test', () => {
   beforeEach(() => {
     cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
       statusCode: 201,
-      body: {
-        movies: [
-          {
-            id: 337401,
-            poster_path:
-              'https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg',
-            backdrop_path:
-              'https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg',
-            title: 'Mulan',
-            average_rating: 5.2727272727272725,
-            release_date: '2020-09-04'
-          },
-          {
-            id: 340102,
-            poster_path:
-              'https://image.tmdb.org/t/p/original//xrI4EnZWftpo1B7tTvlMUXVOikd.jpg',
-            backdrop_path:
-              'https://image.tmdb.org/t/p/original//eCIvqa3QVCx6H09bdeOS8Al2Sqy.jpg',
-            title: 'The New Mutants',
-            average_rating: 4,
-            release_date: '2020-08-26'
-          },
-          {
-            id: 737173,
-            poster_path:
-              'https://image.tmdb.org/t/p/original//opZKcgocttEOAUzqluX3bUbbDew.jpg',
-            backdrop_path: 'https://www.esm.rochester.edu/uploads/NoPhotoAvailable.jpg',
-            title: 'Maratón After',
-            average_rating: 4.333333333333333,
-            release_date: '2020-09-03'
-          }
-        ]
-      }
+      fixture: 'movies'
     }).as('movies')
   })
 
