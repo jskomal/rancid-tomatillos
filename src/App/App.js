31c08b808398ab31beb09ca96c066b357861dace
Jordan Skomal <jskomal@gmail.com>
2022-04-03 17:25:05 -0600

    Refactor all instances of fetching deleting reviews

--- src/App/App.js
+++ src/App/App.js
@@ -6,7 +6,7 @@ import Login from '../Login/Login'
 import MyProfile from '../MyProfile/MyProfile'
 import './App.css'
 import { Route, Switch } from 'react-router-dom'
-import { fetchDataGet } from '../APICalls'
+import { fetchDataGet, fetchDataDelete } from '../APICalls'
 
 export class App extends Component {
   constructor() {
@@ -18,7 +18,8 @@ export class App extends Component {
       isError: false,
       userData: null,
       isLoggedIn: false,
-      errorMsg: ''
+      errorMsg: '',
+      userRatings: null
     }
   }
 
@@ -27,12 +28,10 @@ export class App extends Component {
       .then((res) => {
         if (!res.ok) {
           this.finishLoading()
-          this.setState(
-            {
-              isError: true,
-              errorMsg: 'Server Error, try Rotten Tomatoes instead'
-            }
-          )
+          this.setState({
+            isError: true,
+            errorMsg: 'Server Error, try Rotten Tomatoes instead'
+          })
         } else {
           return res.json()
         }
@@ -66,6 +65,40 @@ export class App extends Component {
 
   toggleLogInStatus = (userData) => {
     this.setState({ isLoggedIn: true, userData: userData })
+  }
+
+  fetchRatings = () => {
+    fetchDataGet(`users/${this.state.userData.id}/ratings`)
+      .then((res) => {
+        if (!res.ok) {
+          this.setState({ errorMsg: 'Something went wrong, try again later' })
+        }
+        return res.json()
+      })
+      .then((data) => {
+        this.setState({ userRatings: data.ratings })
+      })
+  }
+
+  deleteRating = (idToDelete) => {
+    if (this.state.userRatings) {
+      const ratedMovieRatingIDs = this.state.userRatings.map((rating) => rating.id)
+      if (ratedMovieRatingIDs.includes(idToDelete)) {
+        fetchDataDelete(`users/${this.state.userData.id}/ratings/${idToDelete}`)
+          .then((res) => {
+            if (!res.ok) {
+              this.updateErrorMsg('Something went wrong, try again later')
+            }
+          })
+          .then(() => {
+            this.fetchRatings()
+          })
+      }
+    }
+  }
+
+  updateErrorMsg = (newMsg) => {
+    this.setState({ errorMsg: newMsg })
   }
 
   render() {
@@ -112,6 +145,7 @@ export class App extends Component {
                   <h1 className='status-msg'>{this.state.errorMsg}</h1>
                 )}
                 <Login
+                  fetchRatings={this.fetchRatings}
                   finishLoading={this.finishLoading}
                   toggleLogInStatus={this.toggleLogInStatus}
                 />
@@ -135,9 +169,13 @@ export class App extends Component {
                   <h1 className='status-msg'>{this.state.errorMsg}</h1>
                 )}
                 <MyProfile
+                  errorMsg={this.state.errorMsg}
+                  fetchRatings={this.fetchRatings}
                   finishLoading={this.finishLoading}
                   userData={this.state.userData}
                   movies={this.state.movies}
+                  userRatings={this.state.userRatings}
+                  deleteRating={this.deleteRating}
                 />
               </div>
             )
@@ -159,10 +197,16 @@ export class App extends Component {
                   <h1 className='status-msg'>{this.state.errorMsg}</h1>
                 )}
                 <SingleView
+                  updateErrorMsg={this.updateErrorMsg}
+                  errorMsg={this.state.errorMsg}
+                  isLoading={this.state.isLoading}
                   finishLoading={this.finishLoading}
                   currentMovieID={match.params}
                   userData={this.state.userData}
                   isLoggedIn={this.state.isLoggedIn}
+                  userRatings={this.state.userRatings}
+                  deleteRating={this.deleteRating}
+                  fetchRatings={this.fetchRatings}
                 />
               </div>
             )
