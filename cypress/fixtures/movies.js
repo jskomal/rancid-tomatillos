46a561a354f670b48dc963a123ab0db919befbfd
Jordan Skomal <jskomal@gmail.com>
2022-04-03 14:22:50 -0600

    create movies fixture

---
+++ cypress/fixtures/movies.js
@@ -0,0 +1,57 @@
+{
+  movies: [
+    {
+      id: 337401,
+      poster_path: 'https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg',
+      backdrop_path:
+        'https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg',
+      title: 'Mulan',
+      average_rating: 5.2727272727272725,
+      release_date: '2020-09-04'
+    },
+    {
+      id: 340102,
+      poster_path: 'https://image.tmdb.org/t/p/original//xrI4EnZWftpo1B7tTvlMUXVOikd.jpg',
+      backdrop_path:
+        'https://image.tmdb.org/t/p/original//eCIvqa3QVCx6H09bdeOS8Al2Sqy.jpg',
+      title: 'The New Mutants',
+      average_rating: 4,
+      release_date: '2020-08-26'
+    },
+    {
+      id: 737173,
+      poster_path: 'https://image.tmdb.org/t/p/original//opZKcgocttEOAUzqluX3bUbbDew.jpg',
+      backdrop_path: 'https://www.esm.rochester.edu/uploads/NoPhotoAvailable.jpg',
+      title: 'Maratón After',
+      average_rating: 4.333333333333333,
+      release_date: '2020-09-03'
+    },
+    {
+      id: 446893,
+      poster_path: 'https://image.tmdb.org/t/p/original//7W0G3YECgDAfnuiHG91r8WqgIOe.jpg',
+      backdrop_path:
+        'https://image.tmdb.org/t/p/original//qsxhnirlp7y4Ae9bd11oYJSX59j.jpg',
+      title: 'Trolls World Tour',
+      average_rating: 5.5,
+      release_date: '2020-03-12'
+    },
+    {
+      id: 737568,
+      poster_path: 'https://image.tmdb.org/t/p/original//pklyUbh4k1DbHdnsOMASyw7C6NH.jpg',
+      backdrop_path:
+        'https://image.tmdb.org/t/p/original//9pHxv7TX0jOKNgnGMDP6RJ2m1GL.jpg',
+      title: 'The Doorman',
+      average_rating: 5.75,
+      release_date: '2020-10-01'
+    },
+    {
+      id: 592350,
+      poster_path: 'https://image.tmdb.org/t/p/original//zGVbrulkupqpbwgiNedkJPyQum4.jpg',
+      backdrop_path:
+        'https://image.tmdb.org/t/p/original//9guoVF7zayiiUq5ulKQpt375VIy.jpg',
+      title: 'My Hero Academia: Heroes Rising',
+      average_rating: 5.571428571428571,
+      release_date: '2019-12-20'
+    }
+  ]
+}
