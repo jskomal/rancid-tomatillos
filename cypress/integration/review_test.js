885429222f9340f0160bcaa3afb7db1c1d3b6b53
Jordan Skomal <jskomal@gmail.com>
2022-04-03 17:24:38 -0600

    Reformat Test

--- cypress/integration/review_test.js
+++ cypress/integration/review_test.js
@@ -2,11 +2,11 @@ describe('Review/Ratings Tests', () => {
   beforeEach(() => {
     cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
       statusCode: 201,
-      fixture: 'movies',
+      fixture: 'movies'
     }).as('movies')
     cy.intercept('POST', 'https://rancid-tomatillos.herokuapp.com/api/v2/login', {
       statusCode: 201,
       body: { user: { id: 81, name: 'Charlie', email: 'charlie@turing.io' } }
-    })
-      .as('Log In')
+    }).as('Log In')
   })
+})
