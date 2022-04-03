2cf255d3d9272ac5b79497d8f0fb15d8baf3d976
danielsolomon332 <daniel.solomon332@gmail.com>
2022-04-03 15:15:58 -0600

    Begin writing review test

---
+++ cypress/integration/review_test.js
@@ -0,0 +1,12 @@
+describe('Review/Ratings Tests', () => {
+  beforeEach(() => {
+    cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+      statusCode: 201,
+      fixture: 'movies',
+    }).as('movies')
+    cy.intercept('POST', 'https://rancid-tomatillos.herokuapp.com/api/v2/login', {
+      statusCode: 201,
+      body: { user: { id: 81, name: 'Charlie', email: 'charlie@turing.io' } }
+    })
+      .as('Log In')
+  })
