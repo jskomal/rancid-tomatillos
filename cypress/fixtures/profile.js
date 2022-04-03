729576df3f431c02896541bdd34e58adf9945489
Jordan Skomal <jskomal@gmail.com>
2022-04-03 12:59:58 -0600

    Add profile tests

---
+++ cypress/fixtures/profile.js
@@ -0,0 +1,28 @@
+{
+  ratings: [
+    {
+      created_at: '2020-10-18T19:59:16.639Z',
+      id: 3116,
+      movie_id: 446893,
+      rating: 1,
+      updated_at: '2020-10-18T19:59:16.639Z',
+      user_id: 81
+    },
+    {
+      created_at: '2021-02-26T04:25:13.775Z',
+      id: 3498,
+      movie_id: 737568,
+      rating: 7,
+      updated_at: '2021-02-26T04:25:13.775Z',
+      user_id: 81
+    },
+    {
+      created_at: '2021-10-08T17:34:21.748Z',
+      id: 3502,
+      movie_id: 592350,
+      rating: 9,
+      updated_at: '2021-10-08T17:34:21.748Z',
+      user_id: 81
+    }
+  ]
+}
