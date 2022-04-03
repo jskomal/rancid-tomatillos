fb497d72c8d1679d21415155cb2640ad31aace38
Jordan Skomal <jskomal@gmail.com>
2022-04-03 14:23:32 -0600

    Add test for deleting a previous rating

--- cypress/integration/profile_test.js
+++ cypress/integration/profile_test.js
@@ -13,6 +13,11 @@ describe('Profile view tests', () => {
         fixture: 'profile'
       }
     ).as('Get Ratings')
+
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+      statusCode: 201,
+      fixture: 'movies'
+    })
   })
 
   it('Should log in and route to the profile page', () => {
@@ -30,5 +35,30 @@ describe('Profile view tests', () => {
       .click()
       .get('.profile-view')
       .contains("Welcome to Charlie's Rating Room")
+      .get('.movies-to-rate-view')
+      .contains('Mulan')
+      .get('.ratings-view')
+      .contains('Trolls World Tour')
+  })
+
+  it('Should be able to delete a previous rating', () => {
+    cy.intercept(
+      'DELETE',
+      'https://rancid-tomatillos.herokuapp.com/api/v2/users/81/ratings/3116',
+      {
+        statusCode: 204
+      }
+    ).as('Delete Trolls')
+
+    cy.get('#446893Button').click()
+  })
+
+  it('Should be able to route to a unrated Movie by clicking the card', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/340102', {
+      statusCode: 201,
+      fixture: 'mutants'
+    })
+
+    cy.get('#340102').click().get('.single-movie-details').contains('Tagline')
   })
 })
