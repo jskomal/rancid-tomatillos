bfd139556b31d68f406b1c1be83b14a94b6d4a04
danielsolomon332 <daniel.solomon332@gmail.com>
2022-04-04 12:11:56 -0600

    REfactor add id for review

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -5,7 +5,7 @@ import './Modal.css'
 export class Modal extends Component {
   constructor() {
     super()
-    this.state = { ratingValue: '5' }
+    this.state = { ratingValue: '' }
   }
 
   handleInput = (e) => {
@@ -28,6 +28,7 @@ export class Modal extends Component {
             min='1'
             max='10'
             step='1'
+            placeholder='5'
             value={this.state.ratingValue}
             onChange={this.handleInput}
           />
@@ -39,7 +40,7 @@ export class Modal extends Component {
               starRatedColor='goldenrod'
             />
           </div>
-          <button onClick={(e) => this.props.addRating(this.state.ratingValue)}>
+          <button id='submitRatingButton' onClick={(e) => this.props.addRating(this.state.ratingValue)}>
             submit rating
           </button>
           <p className='status-msg'>{this.props.modalErrMsg}</p>
