e491e8135ac5279b4ac7133d11864589b0c8c5ab
danielsolomon332 <daniel.solomon332@gmail.com>
2022-04-04 12:05:04 -0600

    Add login and route test

--- cypress/integration/review_test.js
+++ cypress/integration/review_test.js
@@ -8,5 +8,26 @@ describe('Review/Ratings Tests', () => {
       statusCode: 201,
       body: { user: { id: 81, name: 'Charlie', email: 'charlie@turing.io' } }
     }).as('Log In')
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/737173', {
+      statusCode: 201,
+      fixture: 'maraton'
+    }).as('maraton')
   })
+
+  it('should log in and route to a movie to review', () => {
+    cy.visit('http://localhost:3000/login')
+    .get('[name="email"]')
+    .type('charlie@turing.io')
+    .get('[name="password"]')
+    .type('qwerty')
+    .get('.login-button')
+    .click()
+    .get('.login-view')
+    .wait(2000)
+    .get('img[alt="Maratón After poster"]').click()
+    .get('.review-view')
+    .contains('your rating is:')
+  })
+
+
 })
