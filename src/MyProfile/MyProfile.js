4622f5a886ccd34c9a50b01f02b81246fcfbea67
Jordan Skomal <jskomal@gmail.com>
2022-04-04 14:31:57 -0600

    Remove unused Component import

--- src/MyProfile/MyProfile.js
+++ src/MyProfile/MyProfile.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './MyProfile.css'
 import RatingCard from '../RatingCard/RatingCard'
 import ToRate from '../ToRate/ToRate'
